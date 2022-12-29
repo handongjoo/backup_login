2474aba5cc7b3d5040e7743576d106c2b9466a10
unknown <dongjooyoung@naver.com>
2022-12-29 20:32:19 +0900

    API 작성

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
 
+app.post('/login', (req, res) => {
+    res.send("로그인");
+});
+
+app.post('/logout', (req, res) =>{
+    res.send("로그아웃");
+});
+
+app.post('/register', (req, res) => {
+    res.send("등록"); 
+});
+
+app.get('/users', (req, res) =>{
+    res.send("유저 정보");
+})
+
 app.listen(3000, () => {
     console.log("서버가 열렸습니다.");
 });
